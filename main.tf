fa25a0c3da2faf35460a7eb66c3cb8f4316c3a2f
amshenoy <amodh.shenoy@oracle.com>
2022-05-17 14:57:30 +0530

    Updated files

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-
 provider "oci" {
+  version = "4.75.0"
   auth = "ResourcePrincipal"
+  region = "us-ashburn-1"
 }
 
 resource "oci_objectstorage_bucket" "test_bucket" {
-    #Required
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaay5qjjbyby6nlevnr6nj3hbkdkctsr6phfkny4oqqfymmz2oe3gpa"
-    name = "terraform-test1"
-    namespace = "id204we8d65n"
+  #Required
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaakgh3udl7bpnhy5oiy4pot4ifq7bcnwabrk4p7uoudrbn6elfmkeq"
+  name = "terraform-test1"
+  namespace = "id204we8d65n"
 }
