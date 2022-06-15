f62d7e2250e4982d565e05c1fed3d61c56bce983
amshenoy <amodh.shenoy@oracle.com>
2022-06-15 10:26:06 +0530

    CHanges for testing

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ provider "oci" {
 resource "oci_objectstorage_bucket" "test_bucket" {
   #Required
   compartment_id = "ocid1.compartment.oc1..aaaaaaaakgh3udl7bpnhy5oiy4pot4ifq7bcnwabrk4p7uoudrbn6elfmkeq"
-  name = "terraform-test1"
+  name = "terraform-test-15Jun"
   namespace = "id204we8d65n"
 }
